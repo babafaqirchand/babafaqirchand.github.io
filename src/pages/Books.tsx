4e44f6766c4fcba655ce22db3a6670fd9b4c7861
Christopher Carroll Smith <75859865+chriscarrollsmith@users.noreply.github.com>
2023-07-31 15:50:59 -0400

    Added Other Books

--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -5,21 +5,40 @@ import booksData from '../customizations/books.json';
 import Book from '../types/books';
 
 const Books: React.FC = () => {  
-  const authors = Array.from(new Set(booksData.map(book => book.author)));
+  const mainAuthors = ["Baba Faqir Chand", "Maharishi Shiv Brat Lal Verman"];
+  const copiedBooksData = JSON.parse(JSON.stringify(booksData));
+
+  const authorSections = mainAuthors.map(author => {
+    const authorBooks: Book[] = booksData.filter(book => book.author === author);
+
+    // remove main authors' books from the copiedBooksData
+    authorBooks.forEach(book => {
+      const index = copiedBooksData.findIndex(
+        (copiedBook: Book) => copiedBook.title === book.title && copiedBook.author === book.author
+      );
+      if(index > -1){
+        copiedBooksData.splice(index, 1);
+      }
+    });
+
+    return (
+      <Section id={`${author.replace(/\s/g, '-')}-books`} key={author}>
+        <TabbedBooks books={authorBooks} />
+      </Section>
+    );
+  });
+
+  // remaining copiedBooksData is considered as Other Authors books
+  const otherAuthorsBooks: Book[] = copiedBooksData;
 
   return (
     <main id="skip">
-      {authors.map(author => {
-        const authorBooks: Book[] = booksData.filter(book => book.author === author);
-
-        return (
-          <Section id={`${author.replace(/\s/g, '-')}-books`} key={author}>
-            <TabbedBooks books={authorBooks} />
-          </Section>
-        );
-      })}
+      {authorSections}
+      <Section id="Other-Authors-books">
+        <TabbedBooks books={otherAuthorsBooks} />
+      </Section>
     </main>
-  )
-}
+  );
+};
 
 export default Books;
