55fd08487bc2447e35827bfd905f762003801486
Christopher Carroll Smith <75859865+chriscarrollsmith@users.noreply.github.com>
2023-10-16 18:58:26 -0400

    Support setting inner HTML in biographies

--- src/components/ui/Biography.tsx
+++ src/components/ui/Biography.tsx
@@ -11,7 +11,7 @@ const Biography: React.FC<BiographyProp> = ({ img, alt, bio }) => {
     return(
         <div className="textcontainer overflow-auto">
             <img src={img} alt={alt} className="w-full md:w-1/2 h-auto mb-6 md:mb-0 md:mr-6 float-left" />
-            <p className="text-xl">{bio}</p>
+            <p className="text-xl" dangerouslySetInnerHTML={{ __html: bio }}></p>
         </div>
     );
 }
