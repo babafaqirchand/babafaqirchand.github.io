540baf44e827b1bc49592ed7a8cac806b9e4b98e
Christopher Carroll Smith <75859865+chriscarrollsmith@users.noreply.github.com>
2023-10-19 12:52:18 -0400

    aider: Center video and display title above it.
    # Aider chat conversation:
    
    USER: Edit Video.tsx and Video.css (using Tailwind classNames, with @apply if necessary) so that the video is centered and a title is displayed above the video
    
    ASSISTANT: To center the video and display a title above it, we need to modify both `Video.tsx` and `Video.css`.
    
    In `Video.tsx`, we need to add a new prop for the title and display it above the iframe.
    
    In `Video.css`, we need to add styles to center the video and title.
    
    Here are the changes we need to make:
    
    ```typescript
    src\components\ui\Video.tsx
    <<<<<<< HEAD
    import './Video.css'
    
    interface VideoProps {
        embed_url: string;
      }
    
    const Video: React.FC<VideoProps> = ({embed_url}) => {
    return(
        <iframe
        width="560"
        height="315"
        src={embed_url}
        title="YouTube video player"
        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
        allowFullScreen />
    )
    };
    =======
    import './Video.css'
    
    interface VideoProps {
        embed_url: string;
        title: string;
      }
    
    const Video: React.FC<VideoProps> = ({embed_url, title}) => {
    return(
        <div className="video-container">
            <h2 className="video-title">{title}</h2>
            <iframe
            className="video-frame"
            width="560"
            height="315"
            src={embed_url}
            title="YouTube video player"
            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
            allowFullScreen />
        </div>
    )
    };
    >>>>>>> updated
    ```
    
    ```css
    src\components\ui\Video.css
    <<<<<<< HEAD
    
    =======
    .video-container {
        display: flex;
        flex-direction: column;
        align-items: center;
        justify-content: center;
    }
    
    .video-title {
        margin-bottom: 20px;
    }
    
    .video-frame {
        max-width: 100%;
    }
    >>>>>>> updated
    ```
    
    In the `Video.tsx` file, we added a new prop `title` to the `VideoProps` interface and displayed it above the iframe in a `h2` tag. We also wrapped the `h2` tag and the iframe in a `div` with the class `video-container`.
    
    In the `Video.css` file, we added styles to center the video and title. The `video-container` class uses flexbox to center its children both horizontally and vertically. The `video-title` class adds a margin below the title. The `video-frame` class ensures that the video does not exceed the width of its container.

--- src/components/ui/Video.tsx
+++ src/components/ui/Video.tsx
@@ -2,17 +2,22 @@ import './Video.css'
 
 interface VideoProps {
     embed_url: string;
+    title: string;
   }
 
-const Video: React.FC<VideoProps> = ({embed_url}) => {
+const Video: React.FC<VideoProps> = ({embed_url, title}) => {
 return(
-    <iframe 
-    width="560" 
-    height="315" 
-    src={embed_url} 
-    title="YouTube video player" 
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-    allowFullScreen />
+    <div className="video-container">
+        <h2 className="video-title">{title}</h2>
+        <iframe 
+        className="video-frame"
+        width="560" 
+        height="315" 
+        src={embed_url} 
+        title="YouTube video player" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+        allowFullScreen />
+    </div>
 )
 };
 
