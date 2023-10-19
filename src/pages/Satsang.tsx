43b72aa729eb43bf8e08b94e0fe49eba07223ea1
Christopher Carroll Smith <75859865+chriscarrollsmith@users.noreply.github.com>
2023-10-19 12:55:33 -0400

    aider: Refactored code to display all videos from `satsangData` by mapping over the data and rendering a `Video` component for each item.
    # Aider chat conversation:
    
    ASSISTANT: To display all videos from `satsangData`, we need to map over the data and render a `Video` component for each item.
    
    Here is the change we need to make:
    
    ```typescript
    src\pages\Satsang.tsx
    <<<<<<< HEAD
    import Section from '../components/ui/Section';
    import satsangData from '../customizations/satsang.json';
    import SatsangVideo from '../types/satsang';
    import Video from '../components/ui/Video';
    
    const Satsang: React.FC = () => {
        const satsang: SatsangVideo = satsangData[0];
    
        return (
            <Section id='satsang'>
                <Video title={satsang.title} embed_url={satsang.embed_url} />
            </Section>
        )
    };
    
    export default Satsang;
    =======
    import Section from '../components/ui/Section';
    import satsangData from '../customizations/satsang.json';
    import Video from '../components/ui/Video';
    
    const Satsang: React.FC = () => {
        return (
            <Section id='satsang'>
                {satsangData.map((satsang, index) => (
                    <Video key={index} title={satsang.title} embed_url={satsang.embed_url} />
                ))}
            </Section>
        )
    };
    
    export default Satsang;
    >>>>>>> updated
    ```
    
    In this edit block, we removed the `SatsangVideo` import and the `satsang` constant. Instead, we map over `satsangData` and render a `Video` component for each item. We use the index of each item as the key for each `Video` component.

--- src/pages/Satsang.tsx
+++ src/pages/Satsang.tsx
@@ -1,14 +1,13 @@
 import Section from '../components/ui/Section';
 import satsangData from '../customizations/satsang.json';
-import SatsangVideo from '../types/satsang';
 import Video from '../components/ui/Video';
 
 const Satsang: React.FC = () => {
-    const satsang: SatsangVideo = satsangData[0];
-    
     return (
         <Section id='satsang'>
-            <Video title={satsang.title} embed_url={satsang.embed_url} />
+            {satsangData.map((satsang, index) => (
+                <Video key={index} title={satsang.title} embed_url={satsang.embed_url} />
+            ))}
         </Section>
     )
 };
